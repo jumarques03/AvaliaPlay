67a29f8eb634690e57fbffc5ffed3fd38e6e6865
Júlia Marques <jumarques631@gmail.com>
2025-06-16 13:03:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ jogos = []
 
 @app.post("/adicionar/jogo")
 def adicionar_jogo(jogo: Avaliacao):
+    for jogo_existente in jogos:
+        if jogo_existente.nome == jogo.nome:
+            return {"mensagem": "Já existe este jogo em nosso site!"}
+
     jogos.append(jogo)
     return {"mensagem": "Jogo adicionado!", "jogo": jogo}
 
 @app.get("/jogos")
 def listar_jogos():
-    return {"jogos": jogos}
+    if not jogos:
+        return {"mensagem": "Não há nenhum jogo em nossa lista!"}
+    else: 
+        return {"jogos": jogos}
